2fc895b0efb3e7a9bf03c5424f70fc1c3b2c7cda
Tobyyang1 <140037190+Tobyyang1@users.noreply.github.com>
2024-12-07 10:12:35 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://tobyyang1.github.io/frog11224141/model.json';
 
 
 function preload() {
